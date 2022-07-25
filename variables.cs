bf36e142f67de584d6ac4d29cdbb4328c2f0bfa2
Nathan Lunn <nathandouglaslunn@gmail.com>
2022-07-24 20:09:49 -0400

    learn variables in c#

---
+++ variables.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giraffe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "John";
+            int characterAge = 35;
+
+            Console.WriteLine("There once was a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But he didn't like being " + characterAge);
+
+            Console.ReadLine();
+        }
+    }
+}
